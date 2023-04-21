d266a9ea770c207a29b2b5e801ef0f232b024caa
Travis Lamberte <travis@lamberte.com>
2023-04-21 11:35:09 -0700

    removed orbit files from old project

--- main.js
+++ main.js
@@ -54,12 +54,12 @@ scene.add( sunlight )
 
 // add skybox for stars background
 let materialArray = [];
-let texture_ft = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_ft.png'); //Grab Texture Files Here
-let texture_bk = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_bk.png');
-let texture_up = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_up.png');
-let texture_dn = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_dn.png');
-let texture_rt = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_rt.png');
-let texture_lf = new THREE.TextureLoader().load( '/orbit_images/skybox/space/space_lf.png');
+let texture_ft = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_ft.png'); //Grab Texture Files Here
+let texture_bk = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_bk.png');
+let texture_up = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_up.png');
+let texture_dn = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_dn.png');
+let texture_rt = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_rt.png');
+let texture_lf = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_lf.png');
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft })); //add textures to 'materialArray' List
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
@@ -68,7 +68,7 @@ materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
 materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
 for (let i = 0; i < 6; i++)
   materialArray[i].side = THREE.BackSide; //inverse view for skybox to view from inside box
-let skyboxGeo = new THREE.BoxGeometry( 20000, 20000, 20000);
+let skyboxGeo = new THREE.BoxGeometry( 30000, 30000, 30000);
 let skybox = new THREE.Mesh( skyboxGeo, materialArray );
 scene.add( skybox );
 
