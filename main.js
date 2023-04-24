39334cb99494367717a49c993d0db596dc1703ef
Travis Lamberte <travis@lamberte.com>
2023-04-24 08:06:55 -0700

    add rotating cube to world

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ window.onresize = function(){
 
 //create camera object
 const camera = new THREE.PerspectiveCamera( 39, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.5, 20000 );
-camera.position.set(0, 0, 0)
+camera.position.set(0, 0, -20)
 camera.lookAt(0, 0, 0)
 
 //create renderer
@@ -33,39 +33,30 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
 
 //add ambient light source
-const light = new THREE.AmbientLight( 0xFFFFFF, 0.1 );
+const light = new THREE.AmbientLight( 0xFFFFFF, 0.5 );
 scene.add( light );
 
 //add sun light source
-const sunlight = new THREE.PointLight( 0xFFFFFF, 2, 10000)
-sunlight.castShadow = true;
-scene.add( sunlight )
+// const sunlight = new THREE.PointLight( 0xFFFFFF, 2, 10000)
+// sunlight.castShadow = true;
+// scene.add( sunlight )
 // sunlight.position.set = (0, 0, 0)
 
-// add skybox for room
-// let materialArray = [];
-// let texture_ft = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_ft.png'); //Grab Texture Files Here
-// let texture_bk = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_bk.png');
-// let texture_up = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_up.png');
-// let texture_dn = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_dn.png');
-// let texture_rt = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_rt.png');
-// let texture_lf = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_lf.png');
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft })); //add textures to 'materialArray' List
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
-// for (let i = 0; i < 6; i++)
-//   materialArray[i].side = THREE.BackSide; //inverse view for skybox to view from inside box
-// let skyboxGeo = new THREE.BoxGeometry( 30000, 30000, 30000);
-// let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-// scene.add( skybox );
+//add cube to the world 
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshStandardMaterial({color: 0xFF0000})
+)
+scene.add(cube)
+
+
 
 //ANIMATION FUNCTION
 function animate() {
   requestAnimationFrame( animate );
-  skybox.rotation.z += 0.0005
+  cube.rotation.y += .05
+  cube.rotation.z += .01
+
   renderer.render( scene, camera );
 }
 
