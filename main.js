7326b0e305d844b672ced15fbc9446f0f006994a
Travis Lamberte <travis@lamberte.com>
2023-04-24 06:54:41 -0700

    refactored and commit

--- main.js
+++ main.js
@@ -12,16 +12,6 @@ const scene = new THREE.Scene();
 const canvasContainer = document.querySelector('#canvasContainer') //Grab canvas Container from document
 const sidePanel = document.querySelector('#sidePanel') // add sidePanel to the DOM
 
-
-//window.innerWidth Handler - for differently sized devices
-if (window.innerWidth < 1160){
-    sidePanel.className = "w-1/2 flex flex-col justify-center px-8"
-    canvasContainer.className = "w-3/4"
-}else{
-    sidePanel.className = "w-1/4 flex flex-col justify-center px-8"
-    canvasContainer.className = "w-3/4"
-}
-
 //Window resize handler
 window.onresize = function(){ 
     location.reload();
@@ -52,25 +42,25 @@ sunlight.castShadow = true;
 scene.add( sunlight )
 // sunlight.position.set = (0, 0, 0)
 
-// add skybox for stars background
-let materialArray = [];
-let texture_ft = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_ft.png'); //Grab Texture Files Here
-let texture_bk = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_bk.png');
-let texture_up = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_up.png');
-let texture_dn = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_dn.png');
-let texture_rt = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_rt.png');
-let texture_lf = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_lf.png');
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft })); //add textures to 'materialArray' List
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
-for (let i = 0; i < 6; i++)
-  materialArray[i].side = THREE.BackSide; //inverse view for skybox to view from inside box
-let skyboxGeo = new THREE.BoxGeometry( 30000, 30000, 30000);
-let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-scene.add( skybox );
+// add skybox for room
+// let materialArray = [];
+// let texture_ft = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_ft.png'); //Grab Texture Files Here
+// let texture_bk = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_bk.png');
+// let texture_up = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_up.png');
+// let texture_dn = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_dn.png');
+// let texture_rt = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_rt.png');
+// let texture_lf = new THREE.TextureLoader().load( '/tower_images/skybox/space2/redeclipse_lf.png');
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft })); //add textures to 'materialArray' List
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+// materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+// for (let i = 0; i < 6; i++)
+//   materialArray[i].side = THREE.BackSide; //inverse view for skybox to view from inside box
+// let skyboxGeo = new THREE.BoxGeometry( 30000, 30000, 30000);
+// let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+// scene.add( skybox );
 
 //ANIMATION FUNCTION
 function animate() {
